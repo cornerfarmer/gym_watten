3b4069cb7c0be7e95338b30fd214ff3542225b85
domin1101 <dominik.winkelbauer@outlook.de>
2018-08-07 16:16:32 +0200

    Refactors package structure

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 Cython.Compiler.Options.annotate = True
 setup(
-    ext_modules = cythonize([Extension("envs.watten_env", ["envs/watten_env.pyx"], language="c++")])
+    name='gym_watten',
+    ext_modules = cythonize([Extension("gym_watten.envs.watten_env", ["gym_watten/envs/watten_env.pyx"], language="c++")]),
+    packages=['gym_watten']
 )
